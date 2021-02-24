4ae3d5989b22024060a37612cb3c539ae11fa9c1
nschofield2 <38117276+nschofield2@users.noreply.github.com>
2021-02-23 22:56:47 -0500

    Phase 1

---
+++ Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position.y - player.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //transform.position.y = player.transform.position.y + offset;
+    }
+}
