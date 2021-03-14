ed6261290175f17d9f0c322e29fb59684283e4ba
RithikKalra <71096898+RithikKalra@users.noreply.github.com>
2021-03-14 14:56:28 -0400

    Important Scripts Template

---
+++ Game/Assets/_Scripts/BoundsDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class is used to handle all the main players physical interactions with he world
+//Aaron use this to set the boundaries of the world and other boundaries
+//Should also incorporate all datactions to initiate interactions with enemies and npc's (SuperClass)
+public class BoundsDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game/Assets/_Scripts/MerchantController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class is used to handle player interactions with the merchant
+public class MerchantController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
