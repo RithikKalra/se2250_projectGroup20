ca042c3aa350419f696969338b14bc5ab64c2bbc
Aaron-Moses <71098038+Aaron-Moses@users.noreply.github.com>
2021-03-23 02:41:54 -0400

    Boundaries
    Slime and Player both stay within boundaries
    Added houses, Paths, Players

--- Game/Assets/_Scripts/SlimeMovement.cs
+++ Game/Assets/_Scripts/SlimeMovement.cs
@@ -7,6 +7,8 @@ public class SlimeMovement : MonoBehaviour
     public GameObject Marker;
     public GameObject parent;
     private Vector3 TargetLocation;
+    private int horizontal = 0;
+    private int vertical = 0;
 
     public void SelectTarget(){
         if(Marker!=null){
@@ -17,21 +19,46 @@ public class SlimeMovement : MonoBehaviour
         int randomizer = random.Next(1, 5);
         if (randomizer == 1)
             {
+                
+                if (horizontal > 2){
+                    TargetLocation= new Vector3(-1f, 0f, 0f);
+                    horizontal = horizontal -1;
+                } else {
                 TargetLocation= new Vector3(1f, 0f, 0f);
+                horizontal = horizontal +1;
+                }
                 
             }
         else if (randomizer == 2)
             {
+                if (horizontal < -2){
+                    TargetLocation= new Vector3(1f, 0f, 0f);
+                    horizontal = horizontal +1;
+                } else {
                 TargetLocation= new Vector3(-1f, 0f, 0f);
+                horizontal = horizontal - 1;
+            }
             }
         else if (randomizer == 3)
             {
-                TargetLocation= new Vector3(0f, 1f, 0f);
+                if (vertical > 2){
+                TargetLocation= new Vector3(0f, -1f, 0f);
+                vertical = vertical -1;
+            } else {
+                TargetLocation = new Vector3(0f, 1f, 0f);
+                vertical = vertical + 1;
+            }
             }
         
         else //heelo
             {
-                TargetLocation= new Vector3(0f, -1f, 0f);
+                if (vertical < -2){
+                TargetLocation= new Vector3(0f, 1f, 0f);
+                vertical = vertical +1;
+            } else {
+                TargetLocation = new Vector3(0f, -1f, 0f);
+                vertical = vertical -1;
+            }
             }
         TargetLocation+=parent.transform.position;
         Marker=Instantiate(Marker, TargetLocation, Quaternion.identity);
