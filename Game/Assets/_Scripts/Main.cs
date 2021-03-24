0a10c8443e65c973133f903959b48db6bb55d1ba
RithikKalra <71096898+RithikKalra@users.noreply.github.com>
2021-03-24 15:20:10 -0400

    Spawns In Slimes from prefab but they dont move

--- Game/Assets/_Scripts/Main.cs
+++ Game/Assets/_Scripts/Main.cs
@@ -8,7 +8,34 @@ using UnityEngine;
 //The script is attached to the MainCamera
 public class Main : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] slimes;
+    public float enemySpawnPerSecond = 0.5f;
+
+    private int slimeNum = 2;
+
+    void Awake()
+    {
+        Invoke("SpawnEnemy", 0.5f / enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy()
+    {
+        //Spawns in a maximum of 3 slimes at any given time
+
+        if (slimeNum < 3)
+        {
+            GameObject go = Instantiate<GameObject>(slimes[slimeNum]);
+            slimeNum++;
+
+            //Initial position of spawned enemy
+            Vector3 pos = new Vector3(-18f, 10.5f, 0f);
+            go.transform.position = pos;
+
+            Invoke("SpawnEnemy", 0.5f / enemySpawnPerSecond);
+        }  
+    }
+
+        // Start is called before the first frame update
     void Start()
     {
         
