e8016de0d23128ae0466d9961a503044f1a5b8cb
RithikKalra <71096898+RithikKalra@users.noreply.github.com>
2021-03-29 14:54:41 -0400

    Fix Spawn Bug

--- Game/Assets/_Scripts/Main.cs
+++ Game/Assets/_Scripts/Main.cs
@@ -7,35 +7,8 @@ using UnityEngine;
 //Will also handle other main scripts within the game
 //The script is attached to the MainCamera
 public class Main : MonoBehaviour
-{
-    public GameObject[] slimes;
-    public float enemySpawnPerSecond = 0.5f;
-
-    private int slimeNum = 2;
-
-    void Awake()
-    {
-        Invoke("SpawnEnemy", 0.5f / enemySpawnPerSecond);
-    }
-
-    public void SpawnEnemy()
-    {
-        //Spawns in a maximum of 3 slimes at any given time
-
-        if (slimeNum < 3)
-        {
-            GameObject go = Instantiate<GameObject>(slimes[slimeNum]);
-            slimeNum++;
-
-            //Initial position of spawned enemy
-            Vector3 pos = new Vector3(-18f, 10.5f, 0f);
-            go.transform.position = pos;
-
-            Invoke("SpawnEnemy", 0.5f / enemySpawnPerSecond);
-        }  
-    }
-
-        // Start is called before the first frame update
+{ 
+    // Start is called before the first frame update
     void Start()
     {
         
